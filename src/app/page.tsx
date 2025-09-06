a04368be9d60872eb931b8f1c64b210989e81491
juji <jujiyangasli@gmail.com>
2025-09-07 01:21:33 +0700

    performance using drop-shadow vs text-shadow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import styles from "@/app/page.module.css";
-// import { BgCanvas } from "@/components/bg-canvas";
-import { FpsChecker } from "@/components/global/fps-checker";
+import { BgCanvas } from "@/components/bg-canvas";
+// import { FpsChecker } from "@/components/global/fps-checker";
 import { Hero } from "@/components/sections/hero";
 
 export default function Home() {
   return (
     <>
-      {/* <BgCanvas /> */}
-      <FpsChecker />
+      <BgCanvas />
+      {/* <FpsChecker /> */}
       <main className={styles.main}>
         <Hero />
+        {/* <p>More content coming soon...</p> */}
       </main>
     </>
   );
