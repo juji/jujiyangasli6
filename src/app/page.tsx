76d9b4da26e0bd40e408b107e64316235d59eff4
juji <jujiyangasli@gmail.com>
2025-09-06 23:08:04 +0700

    fps counter: remove bg-canvas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import styles from "@/app/page.module.css";
-import { BgCanvas } from "@/components/bg-canvas";
+// import { BgCanvas } from "@/components/bg-canvas";
 import { FpsChecker } from "@/components/global/fps-checker";
 import { Hero } from "@/components/sections/hero";
 
 export default function Home() {
   return (
     <>
-      <BgCanvas />
+      {/* <BgCanvas /> */}
       <FpsChecker />
       <main className={styles.main}>
         <Hero />
