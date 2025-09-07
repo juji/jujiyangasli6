56c7546d00a1ec8badcc77a3aa9fd8f1b21f9f79
juji <jujiyangasli@gmail.com>
2025-09-08 00:29:52 +0700

    block gallery

--- src/app/works/[slug]/work.tsx
+++ src/app/works/[slug]/work.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import Markdown from "react-markdown";
 import type { Work } from "@/data/works/types";
 import { setHeaderShown } from "@/lib/headerVisibility";
-import { PhotoSwipeGallery } from "./photoswipe-gallery";
+import { BlockGallery } from "./block-gallery";
 
 import styles from "./style.module.css";
 
@@ -28,20 +28,50 @@ export function WorkPage({ content, work }: { content: string; work: Work }) {
     window.scrollTo(0, 0);
   }, []);
 
+  const workTextRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function onResize() {
+      if (!workTextRef.current) return;
+      const rect = workTextRef.current.getBoundingClientRect();
+      const vh = Math.max(
+        document.documentElement.clientHeight || 0,
+        window.innerHeight || 0,
+      );
+      if (rect.bottom > vh) {
+        console.log("setting top sticky", rect.bottom, vh);
+        workTextRef.current.style.setProperty(
+          "--top-sticky-offset",
+          `-${rect.bottom - vh + 48}px`,
+        );
+      } else {
+        console.log("resetting top sticky", rect.bottom, vh);
+        workTextRef.current.style.setProperty("--top-sticky-offset", `0px`);
+      }
+    }
+
+    onResize();
+    window.addEventListener("resize", onResize);
+    return () => window.removeEventListener("resize", onResize);
+  }, []);
+
   return (
     <div className={styles.workPage}>
       <div className={styles.workPageContent}>
-        <h1>{work.title}</h1>
-        <a href={work.url} target="_blank" rel="noopener noreferrer">
-          {work.url}
-        </a>
-        <br />
-        <br />
-        <div className={styles.workContentText}>
-          <Markdown>{content}</Markdown>
+        <div className={styles.workText} ref={workTextRef}>
+          <h1>{work.title}</h1>
+          <a href={work.url} target="_blank" rel="noopener noreferrer">
+            {work.url}
+          </a>
+          <br />
+          <br />
+          <div className={styles.workContentText}>
+            <Markdown>{content}</Markdown>
+          </div>
         </div>
-
-        <PhotoSwipeGallery images={work.images} title={work.title} />
+        <div className={styles.workGallery}>
+          <BlockGallery images={work.images} title={work.title} />
+        </div>
       </div>
     </div>
   );
