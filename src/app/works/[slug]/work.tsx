683ad521d9b60a4811d01f688fbe2ff0c0ffdef5
juji <jujiyangasli@gmail.com>
2025-09-07 20:02:15 +0700

    lightgallery setup

--- src/app/works/[slug]/work.tsx
+++ src/app/works/[slug]/work.tsx
@@ -9,21 +9,18 @@ import { LightGallery } from "./lightgallery";
 import styles from "./style.module.css";
 
 export function WorkPage({ content, work }: { content: string; work: Work }) {
-  const workPageRef = useRef<HTMLDivElement>(null);
-
   useEffect(() => {
     setHeaderShown(true, true);
 
-    // set project color css variable on work page
-    if (!workPageRef.current) return;
-    workPageRef.current.style.setProperty(
+    // set project color css variable on root
+    document.documentElement.style.setProperty(
       "--project-color",
       work.gradientColor,
     );
   }, [work.gradientColor]);
 
   return (
-    <div className={styles.workPage} ref={workPageRef}>
+    <div className={styles.workPage}>
       <div className={styles.workPageContent}>
         <h1>{work.title}</h1>
         <a href={work.url} target="_blank" rel="noopener noreferrer">
