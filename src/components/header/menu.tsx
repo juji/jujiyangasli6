754955202d97f77e262e58929e36f9fabf824a39
juji <jujiyangasli@gmail.com>
2025-09-08 14:25:17 +0700

    menu aaaanimation

--- src/components/header/menu.tsx
+++ src/components/header/menu.tsx
@@ -1,22 +1,97 @@
 "use client";
-import { usePathname } from "next/navigation";
+
+import Link from "next/link";
+import type { RefObject } from "react";
 import { useRef } from "react";
+import { useOnClickOutside } from "usehooks-ts";
 import styles from "./menu.module.css";
 
 export function Menu() {
-  const pathname = usePathname();
-  const _isHome = pathname === "/";
   const menuRef = useRef<HTMLDivElement>(null);
 
-  function onTouchStart() {
+  function open() {
     if (!menuRef.current) return;
-    menuRef.current.classList.toggle(styles.opened);
+    menuRef.current.classList.add(styles.opened);
   }
 
+  function close() {
+    if (!menuRef.current) return;
+    menuRef.current.classList.remove(styles.opened);
+  }
+
+  useOnClickOutside(menuRef as RefObject<HTMLDivElement>, close);
+
   return (
-    <div className={styles.menu} ref={menuRef}>
+    <div className={styles.menu}>
+      <div className={styles.menuList} ref={menuRef}>
+        <h3>Menu</h3>
+        <Link
+          href="/"
+          onClick={() => {
+            if (menuRef.current)
+              menuRef.current.classList.remove(styles.opened);
+          }}
+        >
+          Home
+        </Link>
+        <Link
+          href="/#works"
+          onClick={() => {
+            if (menuRef.current)
+              menuRef.current.classList.remove(styles.opened);
+          }}
+        >
+          Works
+        </Link>
+        <Link
+          href="/#play"
+          onClick={() => {
+            if (menuRef.current)
+              menuRef.current.classList.remove(styles.opened);
+          }}
+        >
+          Play
+        </Link>
+        <Link
+          href="#contact"
+          onClick={() => {
+            if (menuRef.current)
+              menuRef.current.classList.remove(styles.opened);
+          }}
+        >
+          Contact
+        </Link>
+        <hr />
+        <Link
+          href="https://blog.jujiyangasli.com"
+          target="_blank"
+          rel="noreferrer"
+          className="outgoing"
+          onClick={() => {
+            if (menuRef.current)
+              menuRef.current.classList.remove(styles.opened);
+          }}
+        >
+          Blog
+        </Link>
+        <Link
+          href="https://github.com/jujiyangasli"
+          target="_blank"
+          rel="noreferrer"
+          className="outgoing"
+          onClick={() => {
+            if (menuRef.current)
+              menuRef.current.classList.remove(styles.opened);
+          }}
+        >
+          GitHub
+        </Link>
+      </div>
+
+      {/* <div className={styles.menuListAnchor}></div> */}
+
       <button
-        onTouchStart={onTouchStart}
+        onClick={open}
         type="button"
         aria-label="Toggle Menu"
         className={styles.menuButton}
