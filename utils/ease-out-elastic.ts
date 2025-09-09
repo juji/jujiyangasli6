1f7aba960858b3d0414e2a333637dde7450399b1
juji <jujiyangasli@gmail.com>
2025-09-09 18:55:04 +0700

    update ruberry

--- utils/ease-out-elastic.ts
+++ utils/ease-out-elastic.ts
@@ -1,4 +1,13 @@
 import { writeFile } from "fs";
+import path from "path";
+
+const total = 10000
+
+// get directory of this script
+const dir = path.dirname(new URL(import.meta.url).pathname);
+
+// const file = 'ease-out-elastic.css'
+const file = path.join(dir, '../src/app/rubbery.css')
 
 function easeOutElastic(x: number) {
     const c4 = (2 * Math.PI) / 3;
@@ -10,10 +19,7 @@ function easeOutElastic(x: number) {
       : Math.pow(2, -10 * x) * Math.sin((x * 10 - 0.75) * c4) + 1;
 }
 
-
-const total = 10000
 const increment = 100 / total
-
 let str = 'linear(\n'
 for(let i=0; i<=1;i+=increment){
   str += `    ${easeOutElastic(i)} ${ Math.round( i*10000 ) / 100 }%,\n`
@@ -25,10 +31,11 @@ str = `:root {
   --rubbery: ${str};
 }`
 
-writeFile('ease-out-elastic.css', str, (err) => {
+writeFile(file, str, (err) => {
   if (err) {
     console.error('Error writing file:', err);
   } else {
     console.log('File written successfully!');
+    console.log(file);
   }
 });
